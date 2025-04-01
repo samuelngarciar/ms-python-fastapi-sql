401bd717a83faa762004750af00d097cbbbb8ea2
Samuel Garcia <84947793+samuelngarciar@users.noreply.github.com>
2025-03-31 21:48:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api import endpoints
+from app.api import endpoints_health_check
 import logging
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s:  %(message)s')
@@ -8,3 +9,4 @@ logger = logging.getLogger(__name__)
 logger.info("Start app")
 app = FastAPI()
 app.include_router(endpoints.router)
+app.include_router(endpoints_health_check.router)
